56c7c01e525c31d4f680e6007d3d7453ddafcfb8
solarlime <solarlime@outlook.com>
2019-12-15 12:52:48 +0300

    ESLint

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 const characters = [
-    {
-        name: 'Мечник', health: 10
-    },
-    {
-        name: 'Маг', health: 100
-    },
-    {
-        name: 'Маг', health: 0
-    },
-    {
-        name: 'Лучник', health: 0
-    },
+  {
+    name: 'Мечник', health: 10,
+  },
+  {
+    name: 'Маг', health: 100,
+  },
+  {
+    name: 'Маг', health: 0,
+  },
+  {
+    name: 'Лучник', health: 0,
+  },
 ];
 
-const alive = characters.filter(item => item.health > 0);
+const alive = characters.filter((item) => item.health > 0);
